246796914205fc9eb2703f4f91d84712d0093085
amamshah <amit.a.shah@oracle.com>
2018-09-26 07:50:04 +0530

    Updated script to create one security list  per subnet

--- oci_tenancy/base_tf_creation/create_terraform_seclist.py
+++ oci_tenancy/base_tf_creation/create_terraform_seclist.py
@@ -13,14 +13,14 @@ import ConfigParser
 
 parser = argparse.ArgumentParser(description="Create a terraform sec list resource with name \"name-cidr\" for each subnet identified in the subnet input file.  This creates open egress (0.0.0.0/0) and All protocols within subnet ingress rules.  Any other rules should be put in manually.")
 parser.add_argument("file",help="Full Path to the Subnet file. See readme for format example ")
-parser.add_argument("outfile",help="Output Filename")
+#parser.add_argument("outfile",help="Output Filename")
 parser.add_argument("--omcs",help="Input Fileis of pattern \"name-cidr\" ",action="store_true")
 
 if len(sys.argv)==1:
         parser.print_help()
         sys.exit(1)
 
-if len(sys.argv)<3:
+if len(sys.argv)<2:
         parser.print_help()
         sys.exit(1)
 
@@ -33,10 +33,9 @@ config.read('oci-tf.properties')
 
 filename = args.file
 
-outfile = args.outfile
+#outfile = args.outfile
 
 fname = open(filename,"r")
-oname = open(outfile,"w")
 
 ADS = ["AD1","AD2","AD3"]
 
@@ -54,62 +53,53 @@ seclists_per_subnet = int(sps)
 
 
 for line in fname:
-	i = 0
-	# print "before while "
-	if not line.startswith('#') : 
-		while i < seclists_per_subnet :
-			# print "While " + str(i)
-			# print "line = " + line
-			# print "Here:  " + str(seclists_per_subnet)
-			if args.omcs :
+        i = 0
+        # print "before while "
+        if not line.startswith('#') :
 
-				name_sub,AD,pubpvt,dhcp = line.split(',')
-				# line = line.split(",",1)[0]
-				subnet = name_sub.rsplit("-",1)[1].strip()
-				name = name_sub.rsplit("-",1)[0].strip()
+                # print "While " + str(i)
+                # print "line = " + line
+                print "Here:  " + str(seclists_per_subnet)
+                if args.omcs :
+                        name_sub,AD,pubpvt,dhcp = line.split(',')
+                        # line = line.split(",",1)[0]
+                        subnet = name_sub.rsplit("-",1)[1].strip()
+                        name = name_sub.rsplit("-",1)[0].strip()
 
-			else :
+                else :
+                        [name,sub,AD,pubpvt,dhcp] = line.split(',')
+                        linearr = line.split(",")
+                        name = linearr[0].strip()
+                        subnet = linearr[1].strip()
 
-				[name,sub,AD,pubpvt,dhcp] = line.split(',')
-				linearr = line.split(",")
-				name = linearr[0].strip()
-				subnet = linearr[1].strip()
+                while i < seclists_per_subnet :
 
-			ad = ADS.index(AD)
-			ad_name = ad + 1
-			name = name + str(ad_name) + "-" +  str(i+1)
+                        print(' subnet ************************** '+name)
+                        oname = open(name+"_seclist.tf","a")
 
-			display_name = name +  "-" + subnet
-			# print "Name: " + name + " Subnet: " + subnet + "\n"
+                        ad = ADS.index(AD)
+                        ad_name = ad + 1
+                        seclistname = name + str(ad_name) + "-" +  str(i+1)
 
-			tempStr = """
-		resource "oci_core_security_list" \"""" + name +  """"{
-		    compartment_id = "${var.""" + ntk_comp_var + """}"
-		    vcn_id = "${oci_core_virtual_network.""" + vcn_var + """.id}"
-		    display_name = \""""  + display_name.strip() + "\""
-			if i+1 > 1 :
-				tempStr  = tempStr + """
-		}
+                        display_name = seclistname +  "-" + subnet
+                        #print "Name: " + name + " Subnet: " + subnet + "\n"
+                        print ( "Seclist Displayname :::: "+display_name)
+
+                        tempStr = """
+                resource "oci_core_security_list" \"""" + seclistname +  """"{
+                    compartment_id = "${var.""" + ntk_comp_var + """}"
+                    vcn_id = "${oci_core_virtual_network.""" + vcn_var + """.id}"
+                    display_name = \""""  + display_name.strip() + "\""
+
+                        tempStr  = tempStr + """
+                        ###security_rules"""+str(i+1)+"""###
+                }
 """
-			else :
-				tempStr = tempStr + """
-			egress_security_rules {
-				destination = "0.0.0.0/0"
-				protocol = "6"
-			}
-			ingress_security_rules {
-				protocol = "all"
-				source = \"""" + subnet + """"
-			}
-		}
-
-"""
-			# print "Tempstr = " + tempStr
-			oname.write(tempStr)
-			i = i + 1
-	else:
-		i = i + 1
+                        # print "Tempstr = " + tempStr
+                        oname.write(tempStr)
+                        i = i + 1
+        else:
+                i = i + 1
 
 fname.close()
 oname.close()
-
