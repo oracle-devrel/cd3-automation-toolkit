22579a650a2533250d25a6e23e39ee5a08ff978e
Suruchi <suruchi.singla@oracle.com>
2018-10-24 17:10:34 +0530

    Updated code to add routing for SGW, IGW, NGW, LPG and DRG.

--- oci_tenancy/base_tf_creation/create_terraform_route.py
+++ oci_tenancy/base_tf_creation/create_terraform_route.py
@@ -1,135 +1,146 @@
 #!/bin/python
-#Author: Murali Nagulakonda
-#Oracle Consulting
-#murali.nagulakonda.venkata@oracle.com
-
+# Author: Murali Nagulakonda
+# Oracle Consulting
+# murali.nagulakonda.venkata@oracle.com
 
 
 import sys
 import os
 import argparse
 import ConfigParser
+
 ######
 # Required Files
 # Also required is the "oci-tf.properties"
 # List of Subnets - Each Subnet will get its route table.
 # The subnets file can be the same one that is used for creating the sec rules and subnets
-# List of destination:network_entity_id:destination_type
-# destination example: Examples: 10.12.0.0/16 oci-phx-objectstorage
-# destination_type example: Type of destination for the route rule. SERVICE_CIDR_BLOCK should be used if destination is a service cidrBlock. CIDR_BLOCK should be used if destination is IP address range in CIDR notation. It must be provided along with destination
-# Example: 10.26.76.0/23:${oci_core_drg.drg01.id}
 # Outfile
 ######
 
 
-parser = argparse.ArgumentParser(description="Creates a routelist for each subnet. Every route in the routeslist file will be created for each of the subnets." )  
-parser.add_argument("file",help="Full Path to the Subnet file. See readme for format example ")
-parser.add_argument("routeslist",help="Full Path to file")
-parser.add_argument("outfile",help="Output Filename")
-parser.add_argument("--omcs",help="If the File is of OMCS format: \"prod-dmz-lb-ext2-10.89.69.0/24,AD2\"",action="store_true")
+parser = argparse.ArgumentParser(
+    description="Creates a routelist for each subnet. Every route in the routeslist file will be created for each of the subnets.")
+parser.add_argument("subnetfile", help="Full Path to the Subnet file. See readme for format example ")
+parser.add_argument("outfile", help="Output Filename")
+parser.add_argument("--omcs", help="If the File is of OMCS format: \"prod-dmz-lb-ext2-10.89.69.0/24,AD2\"",
+                    action="store_true")
 
-if len(sys.argv)==1:
-        parser.print_help()
-        sys.exit(1)
-if len(sys.argv)<4:
-        parser.print_help()
-        sys.exit(1)
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+if len(sys.argv) < 3:
+    parser.print_help()
+    sys.exit(1)
 
 args = parser.parse_args()
 
 config = ConfigParser.RawConfigParser()
 config.read('oci-tf.properties')
 
-filename = args.file
-routesfile = args.routeslist
+subnet_file = args.subnetfile
 outfile = args.outfile
 
-# print "filename = " + filename + " routefiles  = " + routesfile + " outfile = " + outfile 
-
-fname = open(filename,"r")
-oname = open(outfile,"w")
-rname = open(routesfile,"r")
-
+fname = open(subnet_file, "r")
+oname = open(outfile, "w")
 
 ####
-#ntk_comp_var=ntk_compartment_ocid
-#comp_var=compartment_ocid
-#vcn_var=vcn01
-#####
+# Fetch config properties
+vcn_var = config.get('Default', 'vcn_var')
+ntk_comp_var = config.get('Default', 'ntk_comp_var')
+drg_var = config.get('Default', 'drg_var')
+lpg_var = config.get('Default', 'lpg_var')
+sgw_var = config.get('Default', 'sgw_var')
+ngw_var = config.get('Default', 'ngw_var')
+igw_var = config.get('Default', 'igw_var')
 
-vcn_var = config.get('Default','vcn_var')
-ntk_comp_var = config.get('Default','ntk_comp_var')
-comp_var = config.get('Default','comp_var')
-
-ADS = ["AD1","AD2","AD3"]
-
-
-### Read the Routes file
-
+configure_drg = config.get('Default', 'add_drg_route')
+configure_lpg = config.get('Default', 'add_lpg_route')
+drg_destination = config.get('Default', 'drg_subnet')
+lpg_destination = config.get('Default', 'lpg_subnet')
 
 tempStr = ""
+ruleStr = ""
+
+ADS = ["AD1", "AD2", "AD3"]
+
+if configure_drg == 'true':
+    ruleStr = ruleStr + """
+
+        	route_rules { 
+            destination = \"""" + drg_destination + """\"
+    	    network_entity_id = "${oci_core_drg.""" + drg_var + """.id}"
+    	    destination_type = "CIDR_BLOCK"
+    		}
+    		"""
+if configure_lpg == 'true':
+    ruleStr = ruleStr + """
+
+        	route_rules { 
+            destination = \"""" + lpg_destination + """\"
+    	    network_entity_id = "${oci_core_local_peering_gateway.""" + lpg_var + """.id}"
+    	    destination_type = "CIDR_BLOCK"
+    		}
+    		"""
+
+# Read inpit subnet file
 for line in fname:
-	
-	if not line.startswith('#'):
-		print "processing : " + line
-		subnet = ""
-		name = ""
-                if args.omcs :
+    if not line.startswith('#'):
+        # print "processing : " + line
+        subnet = ""
+        name = ""
+        if args.omcs:
+            name_sub, AD, pubpvt, dhcp, configure_sgw, configure_ngw, configure_igw = line.split(',')
+            subnet = name_sub.rsplit("-", 1)[1].strip()
+            name = name_sub.rsplit("-", 1)[0].strip()
 
-                        name_sub,AD,pubpvt,dhcp = line.split(',')
-                        # line = line.split(",",1)[0]
-                        subnet = name_sub.rsplit("-",1)[1].strip()
-                        name = name_sub.rsplit("-",1)[0].strip()
+        else:
+            [name, sub, AD, pubpvt, dhcp, configure_sgw, configure_ngw, configure_igw] = line.split(',')
+            linearr = line.split(",")
+            name = linearr[0].strip()
+            subnet = linearr[1].strip()
 
-                else :
+        ad = ADS.index(AD)
+        ad_name = ad + 1
+        name = name + str(ad_name)
+        display_name = name + "-" + subnet
 
-                        [name,sub,AD,pubpvt,dhcp] = line.split(',')
-                        linearr = line.split(",")
-                        name = linearr[0].strip()
-                        subnet = linearr[1].strip()
+        tempStr = """
+			resource "oci_core_route_table" \"""" + name + """"{
+			compartment_id = "${var.""" + ntk_comp_var + """}"
+			vcn_id = "${oci_core_vcn.""" + vcn_var + """.id}"
+			display_name = \"""" + display_name.strip() + """\" """ + ruleStr
 
-#		if args.omcs:
-#			#### OMCS Type ####
-#			line = line.split(",",1)[0]
-#			subnet = line.rsplit("-",1)[1].strip()
-#			name = line.rsplit("-",1)[0].strip()
-#		else:
-#			#### non-omcs###
-#			linearr = line.split(",")
-#			name = linearr[0].strip()
-#			subnet = linearr[1].strip()
+        if configure_sgw == 'true':
+            tempStr = tempStr + """
 
-		ad = ADS.index(AD)
-		ad_name = ad + 1
-		name = name + str(ad_name)
-		display_name = name +  "-" + subnet
-		# print "Name: " + name + " Subnet: " + subnet + "\n"
+			route_rules { 
+	     	destination = "${data.oci_core_services.oci_services.services.0.cidr_block}"
+	     	network_entity_id = "${oci_core_service_gateway.""" + sgw_var + """.id}"
+	     	destination_type = "SERVICE_CIDR_BLOCK"
+			}
+			"""
+        if configure_ngw == 'true':
+            tempStr = tempStr + """ 
 
-		tempStr = """
-	resource "oci_core_route_table" \"""" + name +  """"{
-		compartment_id = "${var.""" + ntk_comp_var + """}"
-		vcn_id = "${oci_core_vcn.""" + vcn_var + """.id}"
-		display_name = \""""  + display_name.strip() + """\" """
+			route_rules { 
+		   	destination = "0.0.0.0/0"
+		   	network_entity_id = "${oci_core_nat_gateway.""" + ngw_var + """.id}"
+		   	destination_type = "CIDR_BLOCK"
+			}
+			"""
+        if configure_igw.strip() == 'true':
+            tempStr = tempStr + """
 
-		rname.seek(0,0)
-		for route in rname:
-			if not route.startswith('#'):
-				dest = route.split(":")[0]
-				ntk_ent_id = route.split(":")[1]
-				dest_type = route.split(":")[2]
-				dest_type = dest_type.strip()
-				tempStr = tempStr + """ 
-	route_rules { 
-	     destination = \"""" + dest + """\"
-	     network_entity_id = \"""" + ntk_ent_id + """\"
-	     destination_type = \"""" + dest_type + """\"
+			route_rules { 
+		   	destination = "0.0.0.0/0"
+		   	network_entity_id = "${oci_core_internet_gateway.""" + igw_var + """.id}"
+		   	destination_type = "CIDR_BLOCK"
+			}
+			"""
+        tempStr = tempStr + """
 	}
 	"""
-		tempStr = tempStr + """
-}
-"""
-		oname.write(tempStr)
-
+        oname.write(tempStr)
 
 fname.close()
 oname.close()
