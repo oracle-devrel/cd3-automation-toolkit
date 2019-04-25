2257a15ccfcf8468785475377dd06b66e4900368
Suruchi <suruchi.singla@oracle.com>
2019-04-25 16:13:08 +0530

    updated to accept comaprtment var name

---
+++ oci_tenancy/base_tf_creation_v3.0/create_major_objects.py
@@ -0,0 +1,368 @@
+#!/bin/python
+
+#Author: Suruchi
+#Oracle Consulting
+#suruchi.singla@oracle.com
+
+
+import sys
+import argparse
+import re
+import configparser
+import pandas as pd
+
+######
+# Required Files
+# "csv file- vcn-info.properties"
+# Create the major terraform objects - DRG, IGW, NGW, SGW, LPGs for the VCN
+# Outfile
+######
+
+parser = argparse.ArgumentParser(description="Create major-objects (VCN, IGW, NGW, DRG, LPGs etc for the VCN) terraform file")
+parser.add_argument("propsfile", help="Full Path of props file. eg vcn-info.properties in example folder ")
+parser.add_argument("outfile",help="Output Filename")
+parser.add_argument("--inputCD3", help="input CD3 excel file", required=False)
+
+if len(sys.argv)==2:
+        parser.print_help()
+        sys.exit(1)
+if len(sys.argv)<3:
+        parser.print_help()
+        sys.exit(1)
+
+args = parser.parse_args()
+outfile = args.outfile
+oname = open(outfile,"w")
+
+excel=''
+if(args.inputCD3 is not None):
+    excel=args.inputCD3
+
+
+config = configparser.RawConfigParser()
+config.optionxform = str
+file_read=config.read(args.propsfile)
+
+if(len(file_read)!=1):
+        print(args.propsfile +" doesn't not exist or it could not be opened. Please check input params and try again..")
+        exit(1)
+sections=config.sections()
+
+#Get Global Properties from Default Section
+ntk_comp_var = config.get('Default','ntk_comp_var')
+comp_var = config.get('Default','comp_var')
+drg_ocid = config.get('Default','drg_ocid')
+
+tempStr = ""
+tempStr = tempStr + """
+data "oci_core_services" "oci_services" {
+}"""
+
+# Create VCN transit routing mapping based on Hub-Spoke
+vcn_transit_route_mapping=dict()
+hub_vcn_name=''
+
+# If CD3 exceel file is given as input
+if(excel!=''):
+        df = pd.read_excel(excel, sheet_name='VCNs')
+
+        # Get VCN names from vcn_name column in VCNs sheet of CD3 excel
+        vcns = df['vcn_name']
+
+        for i in df.index:
+                vcn_name=df['vcn_name'][i]
+                hub_spoke_none=df['hub_spoke_none'][i]
+                if (hub_spoke_none == 'hub'):
+                        vcn_transit_route_mapping.setdefault(vcn_name, [])
+                        hub_vcn_name = vcn_name
+        for i in df.index:
+                vcn_name=df['vcn_name'][i]
+                hub_spoke_none=df['hub_spoke_none'][i]
+                if (hub_spoke_none == 'spoke'):
+                        vcn_transit_route_mapping[hub_vcn_name].append(vcn_name)
+
+# If CD3 excel file is not given as input
+else:
+        # Get VCN Info from VCN_INFO section
+        vcns = config.options('VCN_INFO')
+
+        for vcn_name in vcns:
+                vcn_data = config.get('VCN_INFO', vcn_name)
+                vcn_data = vcn_data.split(',')
+                hub_spoke_none = vcn_data[5].strip().lower()
+                if (hub_spoke_none == 'hub'):
+                        vcn_transit_route_mapping.setdefault(vcn_name, [])
+                        hub_vcn_name = vcn_name
+        for vcn_name in vcns:
+            vcn_data = config.get('VCN_INFO', vcn_name)
+            vcn_data = vcn_data.split(',')
+            hub_spoke_none = vcn_data[5].strip().lower()
+            if(hub_spoke_none=='spoke'):
+                vcn_transit_route_mapping[hub_vcn_name].append(vcn_name)
+
+
+#Iterate over all VCNs
+hub_count=0
+
+# If CD3 excel file is given as input
+if(excel!=''):
+        for i in df.index:
+                vcn_name = df['vcn_name'][i]
+                regex = re.compile('[^a-zA-Z0-9]')
+                vcn_dns_label = regex.sub('', vcn_name)
+                vcn_cidr = df['vcn_cidr'][i]
+                vcn_drg = df['drg_required(y|n)'][i]
+                vcn_igw = df['igw_required(y|n)'][i]
+                vcn_ngw = df['ngw_required(y|n)'][i]
+                vcn_sgw = df['sgw_required(y|n)'][i]
+                hub_spoke_none = df['hub_spoke_none'][i]
+                sec_list_per_subnet = df['sec_list_per_subnet'][i]
+                sec_rule_per_seclist = df['sec_rule_per_seclist'][i]
+                add_default_seclist = df['add_default_seclist'][i]
+                if (hub_spoke_none == 'hub' and vcn_drg != 'y'):
+                        print("VCN marked as Hub should have DRG configured..Modify the input file and try again")
+                        exit(1)
+                if (hub_spoke_none == 'hub'):
+                        hub_count = hub_count + 1
+                if (hub_count > 1):
+                        print("Ideally there should be only one Hub VCN in a region. Modify the input file and try again")
+                        exit(1)
+
+                tempStr = tempStr + """
+resource "oci_core_vcn" \"""" + vcn_name + """" {
+        cidr_block = \"""" + vcn_cidr + """"
+        compartment_id = "${var.""" + ntk_comp_var + """}"
+        display_name = \"""" + vcn_name + """"
+        dns_label = \"""" + vcn_dns_label + """"
+}
+"""
+                if vcn_igw == "y":
+                        igw_name = vcn_name + "_igw"
+                        tempStr = tempStr + """
+resource "oci_core_internet_gateway" \"""" + igw_name + """" {
+        compartment_id = "${var.""" + ntk_comp_var + """}"
+        display_name = \"""" + igw_name + """"
+        vcn_id = "${oci_core_vcn.""" + vcn_name + """.id}"
+}
+"""
+
+                if vcn_drg == "y":
+                        drg_name = vcn_name + "_drg"
+                        drg_display = "DRG"
+                        rt_var = drg_name + "_rt"
+
+                        # Create new DRG
+                        if (drg_ocid == ''):
+                                tempStr = tempStr + """
+resource "oci_core_drg" \"""" + drg_name + """" {
+        compartment_id = "${var.""" + ntk_comp_var + """}"
+        display_name = \"""" + drg_display + """"
+}
+resource "oci_core_drg_attachment" "drg_attachment" {
+        drg_id = "${oci_core_drg.""" + drg_name + """.id}"
+        vcn_id = "${oci_core_vcn.""" + vcn_name + """.id}"
+"""
+                        # Use existing DRG
+                        if (drg_ocid != ''):
+                                tempStr = tempStr + """
+resource "oci_core_drg_attachment" "drg_attachment" {
+        drg_id = \"""" + drg_ocid + """"
+        vcn_id = "${oci_core_vcn.""" + vcn_name + """.id}"
+"""
+                        if (hub_spoke_none == 'hub'):
+                                tempStr = tempStr + """
+        route_table_id = "${oci_core_route_table.""" + rt_var + """.id}"
+}
+                """
+                        else:
+                                tempStr = tempStr + """
+}"""
+                if vcn_sgw == "y":
+                        sgw_name = vcn_name + "_sgw"
+                        tempStr = tempStr + """
+resource "oci_core_service_gateway"  \"""" + sgw_name + """" {
+        services {
+        service_id = "${data.oci_core_services.oci_services.services.0.id}"
+        }
+        display_name = \"""" + sgw_name + """"
+        vcn_id = "${oci_core_vcn.""" + vcn_name + """.id}"
+        compartment_id = "${var.""" + ntk_comp_var + """}"
+}
+"""
+                if vcn_ngw == 'y':
+                        ngw_name = vcn_name + "_ngw"
+                        tempStr = tempStr + """
+resource "oci_core_nat_gateway" \"""" + ngw_name + """" {
+        display_name = \"""" + ngw_name + """"
+        vcn_id = "${oci_core_vcn.""" + vcn_name + """.id}"
+        compartment_id = "${var.""" + ntk_comp_var + """}"
+}
+"""
+
+# If CD3 excel file is given as input
+else:
+        for vcn_name in vcns:
+                vcn_data=config.get('VCN_INFO',vcn_name)
+                vcn_data=vcn_data.split(',')
+
+                vcn_cidr=vcn_data[0].strip().lower()
+                regex = re.compile('[^a-zA-Z0-9]')
+                vcn_dns_label = regex.sub('', vcn_name)
+                vcn_drg=vcn_data[1].strip().lower()
+                vcn_igw = vcn_data[2].strip().lower()
+                vcn_ngw = vcn_data[3].strip().lower()
+                vcn_sgw = vcn_data[4].strip().lower()
+                hub_spoke_none = vcn_data[5].strip().lower()
+
+                if(hub_spoke_none=='hub' and vcn_drg!='y'):
+                        print("VCN marked as Hub should have DRG configured..Modify the input file and try again")
+                        exit(1)
+                if(hub_spoke_none=='hub'):
+                        hub_count=hub_count+1
+                if(hub_count>1):
+                        print("Ideally there should be only one Hub VCN in a region. Modify the input file and try again")
+                        exit(1)
+
+                tempStr = tempStr + """
+resource "oci_core_vcn" \"""" + vcn_name + """" {
+	cidr_block = \"""" + vcn_cidr + """"
+	compartment_id = "${var.""" + ntk_comp_var + """}"
+	display_name = \"""" + vcn_name + """"
+	dns_label = \"""" + vcn_dns_label + """"
+}
+"""
+        if vcn_igw == "y":
+                igw_name=vcn_name+"_igw"
+                tempStr = tempStr + """
+resource "oci_core_internet_gateway" \"""" + igw_name + """" {
+	    compartment_id = "${var.""" + ntk_comp_var + """}"
+	    display_name = \"""" + igw_name + """"
+	    vcn_id = "${oci_core_vcn.""" + vcn_name + """.id}"
+}
+"""
+
+        if vcn_drg == "y":
+                drg_name=vcn_name+"_drg"
+                drg_display="DRG"
+                rt_var=drg_name+"_rt"
+
+                #Create new DRG
+                if(drg_ocid==''):
+                        tempStr = tempStr + """
+resource "oci_core_drg" \"""" + drg_name + """" {
+        compartment_id = "${var.""" + ntk_comp_var + """}"
+        display_name = \"""" + drg_display + """"
+}
+resource "oci_core_drg_attachment" "drg_attachment" {
+        drg_id = "${oci_core_drg.""" + drg_name + """.id}"
+        vcn_id = "${oci_core_vcn.""" + vcn_name + """.id}"
+"""
+                #Use existing DRG
+                if(drg_ocid!=''):
+                        tempStr=tempStr+"""
+resource "oci_core_drg_attachment" "drg_attachment" {
+        drg_id = \"""" + drg_ocid + """"
+        vcn_id = "${oci_core_vcn.""" + vcn_name + """.id}"
+"""
+                if(hub_spoke_none=='hub'):
+                    tempStr=tempStr+"""
+        route_table_id = "${oci_core_route_table.""" + rt_var + """.id}"
+}
+"""
+                else:
+                        tempStr=tempStr+"""
+}"""
+        if vcn_sgw == "y":
+                sgw_name = vcn_name + "_sgw"
+                tempStr = tempStr + """
+resource "oci_core_service_gateway"  \"""" + sgw_name + """" {
+        services {
+        service_id = "${data.oci_core_services.oci_services.services.0.id}"
+        }
+        display_name = \"""" + sgw_name + """"
+        vcn_id = "${oci_core_vcn.""" + vcn_name + """.id}"
+        compartment_id = "${var.""" + ntk_comp_var + """}"
+}
+"""
+        if vcn_ngw == 'y':
+                ngw_name = vcn_name + "_ngw"
+                tempStr = tempStr + """
+resource "oci_core_nat_gateway" \"""" + ngw_name + """" {
+        display_name = \"""" + ngw_name + """"
+        vcn_id = "${oci_core_vcn.""" + vcn_name + """.id}"
+        compartment_id = "${var.""" + ntk_comp_var + """}"
+}
+"""
+#Create LPGs as per Section VCN_PEERING
+peering_dict = dict(config.items('VCN_PEERING'))
+ocs_vcn_lpg_ocids=peering_dict['ocs_vcn_lpg_ocid']
+ocs_vcn_lpg_ocids=ocs_vcn_lpg_ocids.split(",")
+peering_dict.pop('ocs_vcn_lpg_ocid')
+peering_dict.pop('ocs_vcn_cidr')
+peering_dict.pop('add_ping_sec_rules_onprem')
+peering_dict.pop('add_ping_sec_rules_vcnpeering')
+
+for left_vcn,value in peering_dict.items():
+        right_vcns=value.split(",")
+        for right_vcn in right_vcns:
+                #Create LPG for VCN on left and peer with OCS VCN
+                if(right_vcn=='ocs_vcn'):
+                        lpg_name = left_vcn + "_ocs_lpg"
+                        tempStr = tempStr + """
+resource "oci_core_local_peering_gateway"  \"""" + lpg_name + """" {
+        display_name = \"""" + lpg_name + """"
+        vcn_id = "${oci_core_vcn.""" + left_vcn + """.id}"
+        compartment_id = "${var.""" + ntk_comp_var + """}"
+"""
+                        if(ocs_vcn_lpg_ocids[0]!=''):
+                                tempStr=tempStr+"""
+        peer_id = \"""" + ocs_vcn_lpg_ocids[0] + """"
+}
+"""
+                                ocs_vcn_lpg_ocids.pop(0)
+                        else:
+                                tempStr=tempStr+"""
+}
+"""
+                else:
+                        #create LPG for VCNs on right
+                        lpg_name=right_vcn+"_"+left_vcn+"_lpg"
+                        tempStr = tempStr + """
+resource "oci_core_local_peering_gateway"  \"""" + lpg_name + """" {
+        display_name = \"""" + lpg_name + """"
+        vcn_id = "${oci_core_vcn.""" + right_vcn + """.id}"
+        compartment_id = "${var.""" + ntk_comp_var + """}"
+"""
+                        if(right_vcn==hub_vcn_name and left_vcn in vcn_transit_route_mapping[hub_vcn_name]):
+                                rt_var=lpg_name+"_rt"
+                                tempStr=tempStr+"""
+        route_table_id = "${oci_core_route_table.""" + rt_var + """.id}"
+}
+"""
+                        else:
+                                tempStr=tempStr+"""
+}
+"""
+                        #create LPG for VCN on left corresponding to above and establish peering
+                        lpg_name=left_vcn+"_"+right_vcn+"_lpg"
+                        peer_lpg_name=right_vcn+"_"+left_vcn+"_lpg"
+                        tempStr = tempStr + """
+resource "oci_core_local_peering_gateway"  \"""" + lpg_name + """" {
+        display_name = \"""" + lpg_name+ """"
+        vcn_id = "${oci_core_vcn.""" + left_vcn + """.id}"
+        compartment_id = "${var.""" + ntk_comp_var + """}"
+        peer_id = "${oci_core_local_peering_gateway.""" + peer_lpg_name + """.id}"
+"""
+                        if (left_vcn == hub_vcn_name and right_vcn in vcn_transit_route_mapping[hub_vcn_name]):
+                                rt_var = lpg_name + "_rt"
+                                tempStr = tempStr + """
+        route_table_id = "${oci_core_route_table.""" + rt_var + """.id}"
+}
+                        """
+                        else:
+                                tempStr = tempStr + """
+}
+"""
+oname.write(tempStr)
+oname.close()
+
---
+++ oci_tenancy/base_tf_creation_v3.0/create_terraform_subnet.py
@@ -0,0 +1,220 @@
+#!/bin/python
+#Author: Suruchi
+#Oracle Consulting
+#suruchi.singla@oracle.com
+
+######
+# Required Files
+# Properties File: vcn-info.properties"
+# Code will read input subnet file name for each vcn from properties file
+# Subnets file will contain info about each subnet
+# Outfile
+######
+
+import sys
+import re
+import os
+import argparse
+import configparser
+import pandas as pd
+
+
+parser = argparse.ArgumentParser(description="Takes in a list of subnet names with format \"name,subnet CIDR,Availability Domain, Public|Private subnet,dhcp-options\". "
+											 "Create terraform files for subnets.")
+parser.add_argument("propsfile", help="Full Path of properties file. eg vcn-info.properties in example folder")
+parser.add_argument("outfile",help="Output Filename")
+parser.add_argument("--omcs",help="If the File is of OMCS format: \"prod-dmz-lb-ext2-10.89.69.0/24,AD2\"",action="store_true")
+parser.add_argument("--inputCD3", help="input CD3 excel file", required=False)
+
+
+if len(sys.argv)==2:
+        parser.print_help()
+        sys.exit(1)
+
+if len(sys.argv)<3:
+        parser.print_help()
+        sys.exit(1)
+
+args = parser.parse_args()
+outfile = args.outfile
+oname = open(outfile,"w")
+fname = None
+
+excel=''
+if(args.inputCD3 is not None):
+    excel=args.inputCD3
+
+config = configparser.RawConfigParser()
+config.optionxform = str
+config.read(args.propsfile)
+sections=config.sections()
+
+#Get Global Properties from Default Section
+ntk_comp_var = config.get('Default','ntk_comp_var')
+comp_var = config.get('Default','comp_var')
+
+
+tempStr = ""
+tempStr = tempStr + """
+data "oci_identity_availability_domains" "ADs" {
+	  compartment_id = "${var.tenancy_ocid}"
+}"""
+ADS = ["AD1", "AD2", "AD3"]
+
+# If CD3 excel file is given as input
+if(excel!=''):
+	df_vcn = pd.read_excel(excel, sheet_name='VCNs')
+	df_vcn.set_index("vcn_name", inplace=True)
+	df_vcn.head()
+	df = pd.read_excel(excel, sheet_name='Subnets')
+	for i in df.index:
+		#Get VCN data
+		vcn_name=df['vcn_name'][i]
+		vcn_data = df_vcn.loc[vcn_name]
+		sps=vcn_data['sec_list_per_subnet']
+		seclists_per_subnet = int(sps)
+		vcn_add_defaul_seclist=vcn_data['add_default_seclist']
+		#Get subnet data
+		name = df.iat[i, 2]
+		subnet = df.iat[i, 3]
+		AD = df.iat[i, 4]
+		pubpvt = df.iat[i, 5]
+		dhcp=df.iat[i,6]
+		dhcp = vcn_name + "_" + dhcp
+		compartment_var_name = df.iat[i,0]
+
+		if (AD.strip() != 'Regional'):
+			ad = ADS.index(AD)
+			ad_name_int = ad + 1
+			ad_name = str(ad_name_int)
+			adString = """availability_domain = "${data.oci_identity_availability_domains.ADs.availability_domains.""" + str(ad) + """.name}" """
+		else:
+			ad_name = ""
+			adString = """availability_domain = "" """
+
+		name = name + ad_name
+		display_name = name + "-" + subnet
+		dnslabel = re.sub('-', '', name)
+
+		tempStr = tempStr + """
+resource "oci_core_subnet" \"""" + name + """" {
+	compartment_id = "${var.""" + compartment_var_name + """}" 
+	""" + adString + """			
+	route_table_id      = "${oci_core_route_table.""" + name + """.id}"
+	vcn_id = "${oci_core_vcn.""" + str(vcn_name) + """.id}" """
+
+		seclist_ids = ""
+		if vcn_add_defaul_seclist == "y":
+			seclist_ids = """\"${oci_core_vcn.""" + vcn_name + """.default_security_list_id}","""
+		i = 1
+		while i < seclists_per_subnet:
+			seclist_ids = seclist_ids + """\"${oci_core_security_list.""" + name + "-" + str(i) + """.id}","""
+			i = i + 1
+		while i <= seclists_per_subnet:
+			seclist_ids = seclist_ids + """\"${oci_core_security_list.""" + name + "-" + str(i) + """.id}" """
+			i = i + 1
+		tempStr = tempStr + """
+	security_list_ids   = [ """ + seclist_ids + """ ] 
+	dhcp_options_id     = "${oci_core_dhcp_options.""" + dhcp.strip() + """.id}"
+	display_name               = \"""" + display_name + """"
+	cidr_block                 = \"""" + subnet + """\" """
+		if pubpvt.lower() == "public":
+			tempStr = tempStr + """
+	prohibit_public_ip_on_vnic = false """
+		else:
+			tempStr = tempStr + """
+	prohibit_public_ip_on_vnic = true """
+		tempStr = tempStr + """
+	dns_label           =  \"""" + dnslabel + """"
+}
+"""
+# If CD3 excel file is not given as input
+else:
+	#Get vcn and subnet file info from VCN_INFO section
+	vcns=config.options('VCN_INFO')
+	for vcn_name in vcns:
+		vcn_data = config.get('VCN_INFO', vcn_name)
+		vcn_data = vcn_data.split(',')
+
+
+		sps = vcn_data[8].strip().lower()
+		vcn_add_defaul_seclist = vcn_data[10].strip().lower()
+		vcn_subnet_file = vcn_data[6].strip().lower()
+		if os.path.isfile(vcn_subnet_file) == False:
+			print("input subnet file " + vcn_subnet_file + " for VCN " + vcn_name + " does not exist. Skipping Subnet TF creation for this VCN.")
+			continue
+
+		fname = open(vcn_subnet_file,"r")
+		seclists_per_subnet = int(sps)
+
+		# Read subnet file
+		for line in fname:
+			if not line.startswith('#'):
+				if args.omcs:
+
+					name_sub, AD, pubpvt, dhcp = line.split(',')
+					subnet = name_sub.rsplit("-", 1)[1].strip()
+					name = name_sub.rsplit("-", 1)[0].strip()
+
+				else:
+
+					[name, sub, AD, pubpvt, dhcp, SGW, NGW, IGW] = line.split(',')
+					linearr = line.split(",")
+					name = linearr[0].strip()
+					subnet = linearr[1].strip()
+
+				dhcp=vcn_name+"_"+dhcp
+
+				if(AD.strip()!='Regional'):
+					ad = ADS.index(AD)
+					ad_name_int = ad + 1
+					ad_name=str(ad_name_int)
+					adString="""availability_domain = "${data.oci_identity_availability_domains.ADs.availability_domains.""" + str(ad) + """.name}" """
+				else:
+					ad_name=""
+					adString="""availability_domain = "" """
+
+				name = name + ad_name
+				display_name = name +  "-" + subnet
+				dnslabel = re.sub('-','',name)
+
+
+				tempStr = tempStr+"""
+resource "oci_core_subnet" \"""" + name + """" {
+	compartment_id = "${var.""" + ntk_comp_var + """}" 
+	"""+adString+"""			
+	route_table_id      = "${oci_core_route_table.""" + name + """.id}"
+	vcn_id = "${oci_core_vcn.""" + str(vcn_name) + """.id}" """
+		
+				seclist_ids = ""
+				if vcn_add_defaul_seclist == "y" :
+					seclist_ids = """\"${oci_core_vcn.""" + vcn_name + """.default_security_list_id}","""
+				i = 1
+				while i < seclists_per_subnet :
+					seclist_ids  = seclist_ids + """\"${oci_core_security_list.""" + name + "-" + str(i) + """.id}","""
+					i = i + 1
+				while i <= seclists_per_subnet :
+					seclist_ids  = seclist_ids + """\"${oci_core_security_list.""" + name + "-" + str(i) + """.id}" """
+					i = i + 1
+				tempStr = tempStr + """
+	security_list_ids   = [ """ + seclist_ids + """ ] 
+	dhcp_options_id     = "${oci_core_dhcp_options.""" + dhcp.strip() + """.id}"
+	display_name               = \"""" + display_name + """"
+	cidr_block                 = \"""" + subnet + """\" """
+				if pubpvt.lower() == "public":
+					tempStr = tempStr + """
+	prohibit_public_ip_on_vnic = false """
+				else:
+					tempStr = tempStr + """
+	prohibit_public_ip_on_vnic = true """
+				tempStr = tempStr + """
+	dns_label           =  \"""" + dnslabel + """"
+}
+
+"""
+oname.write(tempStr)
+
+if fname != None:
+	fname.close()
+oname.close()
+
