031b9714575d33eaf94e50ac3ca84245aa042571
Suruchi <suruchi.singla@oracle.com>
2019-08-02 18:55:58 +0530

    updated to add retention days

--- oci_tenancy/oci_automation/utility_scripts/fetch_vol_backups.py
+++ oci_tenancy/oci_automation/utility_scripts/fetch_vol_backups.py
@@ -3,10 +3,12 @@ import argparse
 import sys
 import oci
 import datetime
-#import shutil
+import shutil
+from datetime import timedelta
+import os
 
 
-parser = argparse.ArgumentParser(description="Takes in arg mentioning comaprtment OCID and fetches backups for boot and block vols of all instances in that compartment")
+parser = argparse.ArgumentParser(description="Takes in arg mentioning compartment OCID and fetches backups for boot and block vols of all instances in that compartment")
 parser.add_argument("compartment_file", help="input file containing compartment info")
 parser.add_argument("out_dir", help="output directory for log files having backup info")
 
@@ -19,20 +21,29 @@ compartment_file = args.compartment_file
 out_dir = args.out_dir
 fname = open(compartment_file, "r")
 
-x = str(datetime.date.today())
-#date = x.strftime("%f").strip()
+today = datetime.date.today()
+yesterday = str(today - datetime.timedelta(days = 1))
+
 
 signer = oci.auth.signers.InstancePrincipalsSecurityTokenSigner()
 
 for line in fname:
     if not line.startswith('#'):
+        if ("Log Retention Days" in line):
+            retentionDays = line.split("=")[1].strip()
+            retaindays = int(retentionDays)
+            continue
         linearr = line.split("=")
         comp_name=linearr[0].strip()
         comp_ocid=linearr[1].strip()
 
-        out_file=out_dir+"/backups_"+comp_name+"_"+x+".log"
 
-        outf=open(out_file,"w")
+        out_file=out_dir+"/backups_"+comp_name+".log"
+        yesterday_file=out_file+"_"+yesterday
+        if(os.path.exists(out_file)):
+            shutil.copy(out_file,yesterday_file)
+        outf=open(out_file,"w+")
+
         if("Ryder-Prod" in comp_name):
             storage_client = oci.core.BlockstorageClient(config={'region': 'us-ashburn-1'}, signer=signer)
             compute_client = oci.core.ComputeClient(config={'region': 'us-ashburn-1'}, signer=signer)
@@ -66,10 +77,6 @@ for line in fname:
                     for boot_vol_backup in boot_vol_backups.data:
                         outf.write(boot_vol_backup.display_name+"   |   "+boot_vol_backup.type+" |   "+boot_vol_backup.lifecycle_state+"  |   "+str(boot_vol_backup.time_created)+"    |   "+str(boot_vol_backup.expiration_time))
                         outf.write("\n")
-                        #print(boot_vol_backup.display_name)
-                        #print(boot_vol_backup.lifecycle_state)
-                        #print(boot_vol_backup.expiration_time)
-                        #print(boot_vol_backup.time_created)
 
             block_vol_attachments_list=oci.pagination.list_call_get_all_results(compute_client.list_volume_attachments,compartment_id=comp_ocid,
                                                                             instance_id=instance_ocid)
@@ -89,10 +96,17 @@ for line in fname:
                         outf.write(block_vol_backup.display_name + "   |   " + block_vol_backup.type + " |   "+ block_vol_backup.lifecycle_state + "  |   " + str(block_vol_backup.time_created) + "    |   " + str(block_vol_backup.expiration_time))
                         outf.write("\n")
                     outf.write("\n=====================================================================================\n")
-                    #    print(block_vol_backup.display_name)
-                    #    print(block_vol_backup.lifecycle_state)
-                    #    print(block_vol_backup.expiration_time)
-                    #    print(block_vol_backup.time_created)
+        outf.close()
 
+fname.close()
+retaindays_ago = today - timedelta(days=retaindays)
+file_list=os.listdir(out_dir)
+
+for file in file_list:
+    statbuf = os.stat(out_dir+"/"+file)
+    modificationDate=datetime.date.fromtimestamp((statbuf.st_mtime))
+
+    if(modificationDate < retaindays_ago):
+        os.remove(out_dir+"/"+file)
 
 print("Check output files created at "+out_dir)
