ea0a9e01f7ef8dfe5554885a5f275b14caa27644
shrsubra <shruthi.subramanian@oracle.com>
2020-09-13 20:52:44 +0530

    Policy - comparment cannot be 'null'

--- oci_tenancy/SetUpOCI_Via_TF/Identity/Policies/create_terraform_policies.py
+++ oci_tenancy/SetUpOCI_Via_TF/Identity/Policies/create_terraform_policies.py
@@ -120,7 +120,7 @@ def main():
             if columnname == "Compartment Name":
                 columnname = commonTools.check_column_headers(columnname)
                 compartmentVarName = columnvalue.strip()
-                if compartmentVarName.lower() == 'root' or compartmentVarName.lower() == '' :
+                if compartmentVarName.lower() == 'root':
                     columnvalue = 'tenancy_ocid'
                 else:
                     compartmentVarName = commonTools.check_tf_variable(compartmentVarName)
