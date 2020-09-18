2793e0056d5efcda77ad2e1acd08f309baed85d0
shrsubra <shruthi.subramanian@oracle.com>
2020-09-18 11:34:53 +0530

    Tags Export Code Updated

--- oci_tenancy/SetUpOCI_Via_TF/ExportFromOCI/export_tags_nonGreenField.py
+++ oci_tenancy/SetUpOCI_Via_TF/ExportFromOCI/export_tags_nonGreenField.py
@@ -10,8 +10,6 @@
 import argparse
 import sys
 import oci
-import re
-import json
 from oci.identity import IdentityClient
 import os
 sys.path.append(os.getcwd()+"/..")
@@ -19,17 +17,14 @@ from commonTools import *
 
 compartment_ids={}
 importCommands={}
-oci_obj_names={}
-tf_name_namespace_list = {}
+tf_name_namespace_list = []
 
-def  print_tags(values_for_column_tags,region, ntk_compartment_name, tag, tag_key, tag_default_value, tag_default):
-    tag_default_req = "TRUE"
+def  print_tags(values_for_column_tags,region, ntk_compartment_name, tag, tag_key, tag_default_value, tag_default, tag_default_comp):
     if ( tag_default_value == '' ) :
-     tag_default_req = "FALSE"
+     tag_default_comp = ""
      tag_default_id = ''
     else:
      tag_default_id = tag_default.id
-    validator = ''
     validator = tag_key.validator
     if ( validator is not None):
       validator = str(validator)
@@ -56,8 +51,8 @@ def  print_tags(values_for_column_tags,region, ntk_compartment_name, tag, tag_ke
             values_for_column_tags[col_header].append(tag_key.is_cost_tracking)
         elif (col_header == "Validator"):
             values_for_column_tags[col_header].append(validator)
-        elif (col_header == "Default Tag"):
-            values_for_column_tags[col_header].append(tag_default_req)
+        elif (col_header == "Default Tag Compartment"):
+            values_for_column_tags[col_header].append(tag_default_comp)
         elif (col_header == "Default Tag Value"):
             values_for_column_tags[col_header].append(tag_default_value)
         elif col_header.lower() in commonTools.tagColumns:
@@ -65,12 +60,12 @@ def  print_tags(values_for_column_tags,region, ntk_compartment_name, tag, tag_ke
         else:
             oci_objs = [tag,tag_key,tag_default]
             values_for_column_tags = commonTools.export_extra_columns(oci_objs, col_header, sheet_dict_tags,values_for_column_tags)
-    if (tag.id not in tf_name_namespace_list[region.lower()]):
-        importCommands[region.lower()].write("\nterraform import oci_identity_tag_namespace." + tf_name_namespace + " " + str(tag.id))
-        tf_name_namespace_list[region.lower()].append(tag.id)
-    importCommands[region.lower()].write("\nterraform import oci_identity_tag."+ tf_name_key + ' ' + "tagNamespaces/"+ str(tag.id) +"/tags/\"" + str(tag_key.name) + "\"")
+    if (tag.id not in tf_name_namespace_list):
+        importCommands[region].write("\nterraform import oci_identity_tag_namespace." + tf_name_namespace + " " + str(tag.id))
+        tf_name_namespace_list.append(tag.id)
+    importCommands[region].write("\nterraform import oci_identity_tag."+tf_name_namespace + '-' + tf_name_key + ' ' + "tagNamespaces/"+ str(tag.id) +"/tags/\"" + str(tag_key.name) + "\"")
     if ( tag_default_value != ''):
-        importCommands[region.lower()].write("\nterraform import oci_identity_tag_default."+ tf_name_namespace+'-' +tf_name_key + '-default'+ ' ' + str(tag_default_id))
+        importCommands[region].write("\nterraform import oci_identity_tag_default."+ tf_name_namespace+'-' +tf_name_key + '-default'+ ' ' + str(tag_default_id))
 
 
 def main():
@@ -122,7 +117,6 @@ def main():
     # Get dict for columns from Excel_Columns
     sheet_dict_tags = ct.sheet_dict["Tags"]
 
-
     # Check Compartments
     remove_comps = []
     if (input_compartment_names is not None):
@@ -143,74 +137,83 @@ def main():
     print("Tabs- Tags would be overwritten during export process!!!\n")
 
     # Create backups
-    for reg in ct.all_regions:
-        if (os.path.exists(outdir + "/" + reg + "/tf_import_commands_tags_nonGF.sh")):
-                   commonTools.backup_file(outdir + "/" + reg, "tf_import_tags", "tf_import_commands_tags_nonGF.sh")
-        importCommands[reg] = open(outdir + "/" + reg + "/tf_import_commands_tags_nonGF.sh", "w")
-        importCommands[reg].write("#!/bin/bash")
-        importCommands[reg].write("\n")
-        importCommands[reg].write("terraform init")
+    if (os.path.exists(outdir + "/" + ct.home_region + "/tf_import_commands_tags_nonGF.sh")):
+               commonTools.backup_file(outdir + "/" + ct.home_region, "tf_import_tags", "tf_import_commands_tags_nonGF.sh")
+    importCommands[ct.home_region] = open(outdir + "/" + ct.home_region + "/tf_import_commands_tags_nonGF.sh", "w")
+    importCommands[ct.home_region].write("#!/bin/bash")
+    importCommands[ct.home_region].write("\n")
+    importCommands[ct.home_region].write("terraform init")
 
     # Fetch Tags
     print("\nFetching Tags...")
-    for reg in ct.all_regions:
-        importCommands[reg].write("\n\n######### Writing import for Tags #########\n\n")
-        config.__setitem__("region", ct.region_dict[reg])
-        comp_ocid_done = []
-        tf_name_namespace_list[reg] = []
-        identity = IdentityClient(config,retry_strategy=oci.retry.DEFAULT_RETRY_STRATEGY)
-        validator_con = oci.identity.models.EnumTagDefinitionValidator()
-        region = reg.capitalize()
-        for ntk_compartment_name in ct.ntk_compartment_ids:
-            if ct.ntk_compartment_ids[ntk_compartment_name] not in comp_ocid_done:
-                if (input_compartment_names is not None and ntk_compartment_name not in input_compartment_names):
-                    continue
-                comp_ocid_done.append(ct.ntk_compartment_ids[ntk_compartment_name])
-                tags = oci.pagination.list_call_get_all_results(identity.list_tag_namespaces, compartment_id=ct.ntk_compartment_ids[ntk_compartment_name])
-                tag_defaults = oci.pagination.list_call_get_all_results(identity.list_tag_defaults, compartment_id=ct.ntk_compartment_ids[ntk_compartment_name],lifecycle_state="ACTIVE")
+    importCommands[ct.home_region].write("\n\n######### Writing import for Tags #########\n\n")
+    config.__setitem__("region", ct.region_dict[ct.home_region])
+    comp_ocid_done = []
+    identity = IdentityClient(config,retry_strategy=oci.retry.DEFAULT_RETRY_STRATEGY)
+    region = ct.home_region.lower()
+    for ntk_compartment_name in ct.ntk_compartment_ids:
+        if ct.ntk_compartment_ids[ntk_compartment_name] not in comp_ocid_done:
+            for ntk_compartment_name in ct.ntk_compartment_ids:
+                if ct.ntk_compartment_ids[ntk_compartment_name] not in comp_ocid_done:
+                    if (input_compartment_names is not None and ntk_compartment_name not in input_compartment_names):
+                        continue
+                    comp_ocid_done.append(ct.ntk_compartment_ids[ntk_compartment_name])
+                    tags = oci.pagination.list_call_get_all_results(identity.list_tag_namespaces,
+                                                                    compartment_id=ct.ntk_compartment_ids[
+                                                                        ntk_compartment_name])
+                    tag_defaults = oci.pagination.list_call_get_all_results(identity.list_tag_defaults,
+                                                                            compartment_id=ct.ntk_compartment_ids[
+                                                                                ntk_compartment_name],
+                                                                            lifecycle_state="ACTIVE")
 
-                for tag in tags.data:
-                    tag_keys = oci.pagination.list_call_get_all_results(identity.list_tags, tag_namespace_id= tag.id,lifecycle_state="ACTIVE")
-                    tag_key_check = []
-                    tag_default_check = []
-                    for tag_key in tag_keys.data:
-                       tag_key = identity.get_tag(tag.id, tag_key.name)
-                       tag_key = tag_key.data
-                       tag_key_id = str(tag_key.id)
-                       tag_key_check.append(tag_key_id)
-                       for tag_default in tag_defaults.data :
-                            tag_default_value = ''
-                            if ( tag_key.id == tag_default.tag_definition_id ):
-                                 tag_key_id = str(tag_key.id)
-                                 tag_default_check.append(tag_key_id)
-                                 tag_default_value = tag_default.value
-                                 print_tags(values_for_column_tags, region, ntk_compartment_name, tag, tag_key, tag_default_value, tag_default)
-                    check_non_default_tags = [i for i in tag_key_check + tag_default_check if i not in tag_key_check or i not in tag_default_check]
-                    for tag_check in check_non_default_tags:
+                    for tag in tags.data:
+                        tag_keys = oci.pagination.list_call_get_all_results(identity.list_tags, tag_namespace_id=tag.id,
+                                                                            lifecycle_state="ACTIVE")
+                        tag_key_check = []
+                        tag_default_check = []
                         for tag_key in tag_keys.data:
-                           if ( tag_check in  tag_key.id):
-                                 tag_key = identity.get_tag(tag.id, tag_key.name)
-                                 tag_key = tag_key.data
-                                 tag_default_value = ''
-                                 tag_default = ''
-                                 print_tags(values_for_column_tags, region, ntk_compartment_name, tag, tag_key, tag_default_value, tag_default)
+                            tag_key = identity.get_tag(tag.id, tag_key.name)
+                            tag_key = tag_key.data
+                            tag_key_id = str(tag_key.id)
+                            tag_key_check.append(tag_key_id)
+                            for tag_default in tag_defaults.data:
+                                if (ct.ntk_compartment_ids[ntk_compartment_name] == tag_default.compartment_id):
+                                    tag_default_comp = ntk_compartment_name
+                                    if ("::" in ntk_compartment_name):
+                                        tag_default_comp = ntk_compartment_name
+                                if (tag_key.id == tag_default.tag_definition_id):
+                                    tag_key_id = str(tag_key.id)
+                                    tag_default_check.append(tag_key_id)
+                                    tag_default_value = tag_default.value
+                                    print_tags(values_for_column_tags, region, ntk_compartment_name, tag, tag_key,
+                                               tag_default_value, tag_default, tag_default_comp)
+                        check_non_default_tags = [i for i in tag_key_check + tag_default_check if
+                                                  i not in tag_key_check or i not in tag_default_check]
+                        for tag_check in check_non_default_tags:
+                            for tag_key in tag_keys.data:
+                                if (tag_check in tag_key.id):
+                                    tag_key = identity.get_tag(tag.id, tag_key.name)
+                                    tag_key = tag_key.data
+                                    tag_default_value = ''
+                                    tag_default = ''
+                                    print_tags(values_for_column_tags, region, ntk_compartment_name, tag, tag_key,
+                                               tag_default_value, tag_default, tag_default_comp)
 
-    commonTools.write_to_cd3(values_for_column_tags, cd3file, "Tags")
-    print("Tags exported to CD3\n")
+            commonTools.write_to_cd3(values_for_column_tags, cd3file, "Tags")
+            print("Tags exported to CD3\n")
 
-    os.chdir("../../..")
+            os.chdir("../../..")
 
 
-    for reg in ct.all_regions:
-        importCommands[reg] = open(outdir + "/" + reg + "/tf_import_commands_tags_nonGF.sh", "a")
-        importCommands[reg].write("\n\nterraform plan")
-        importCommands[reg].write("\n")
-        importCommands[reg].close()
-        if ("linux" in sys.platform):
-            dir = os.getcwd()
-            os.chdir(outdir + "/" + reg)
-            os.system("chmod +x tf_import_commands_tags_nonGF.sh")
-            os.chdir(dir)
+    importCommands[ct.home_region] = open(outdir + "/" + ct.home_region + "/tf_import_commands_tags_nonGF.sh", "a")
+    importCommands[ct.home_region].write("\n\nterraform plan")
+    importCommands[ct.home_region].write("\n")
+    importCommands[ct.home_region].close()
+    if ("linux" in sys.platform):
+        dir = os.getcwd()
+        os.chdir(outdir + "/" + ct.home_region)
+        os.system("chmod +x tf_import_commands_tags_nonGF.sh")
+        os.chdir(dir)
 
 
 if __name__=="__main__":
