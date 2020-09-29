8809986e7300d9b7999aaecc55886aeae2e189c0
shrsubra <shruthi.subramanian@oracle.com>
2020-09-29 16:13:38 +0530

    DB

--- oci_tenancy/SetUpOCI_Via_TF/Database/create_terraform_adw_atp.py
+++ oci_tenancy/SetUpOCI_Via_TF/Database/create_terraform_adw_atp.py
@@ -77,8 +77,8 @@ def main():
     for eachregion in uniquereg:
         eachregion = str(eachregion).strip().lower()
         reg_out_dir = outdir + "/" + eachregion
-        if (eachregion in commonTools.endNames):
-            break
+        if (eachregion in commonTools.endNames) or ('nan' in str(eachregion).lower() ):
+            continue
         if eachregion not in ct.all_regions:
             print("\nERROR!!! Invalid Region; It should be one of the regions tenancy is subscribed to..Exiting!")
             exit()
