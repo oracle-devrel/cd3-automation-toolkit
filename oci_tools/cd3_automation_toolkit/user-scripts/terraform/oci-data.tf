2bf8442d446c7305d97ac4ee172314d1945de6d4
Shruthi <shruthi.subramanian@oracle.com>
2022-04-05 15:04:06 +0530

    DRG Data moved to network.tf

--- oci_tools/cd3_automation_toolkit/user-scripts/terraform/oci-data.tf
+++ oci_tools/cd3_automation_toolkit/user-scripts/terraform/oci-data.tf
@@ -40,33 +40,3 @@ output "ads" {
   value = data.oci_identity_availability_domains.availability_domains.availability_domains.*.name
 }
 */
-
-################################
-# Data Block - Network
-# DRG Route Rules and DRG Route Distributions
-################################
-
-data "oci_core_drg_route_tables" "drg_route_tables" {
-  for_each = (var.data_drg_route_tables != null || var.data_drg_route_tables != {}) ? var.data_drg_route_tables : {}
-
-  #Required
-  drg_id = merge(module.drgs.*...)[each.value.drg_id]["drg_tf_id"]
-  filter {
-    name   = "display_name"
-    values = [each.value.values]
-  }
-
-}
-
-
-data "oci_core_drg_route_distributions" "drg_route_distributions" {
-  for_each = (var.data_drg_route_table_distributions != null || var.data_drg_route_table_distributions != {}) ? var.data_drg_route_table_distributions : {}
-
-  #Required
-  drg_id = merge(module.drgs.*...)[each.value.drg_id]["drg_tf_id"]
-  filter {
-    name   = "display_name"
-    values = [each.value.values]
-  }
-
-}
