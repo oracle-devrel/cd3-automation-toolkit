f98bc78b92f1621945f71aac1854b5bae05ba535
Suruchi <suruchi.singla@oracle.com>
2024-03-08 17:33:48 +0530

    Automation Toolkit Release v24.2.0

--- jenkins_install/scriptler/scripts/AdditionalFilters.groovy
+++ jenkins_install/scriptler/scripts/AdditionalFilters.groovy
@@ -49,7 +49,71 @@ for (item in SubOptions.split(",")) {
 
       """
       }
+    if (item.equals("Export Firewall Policy")) {
+        html_to_be_rendered = """
+        ${html_to_be_rendered}
 
+       <tr>
+   <td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"ins_fir_pattern_filter=[\"></td>
+    <td><label title=\"service1-label\" class=\" \">Display Name Pattern Filter for Firewall Policy : </label></td>
+    <td>
+    <input type=\"text\" class=\" \" name=\"value\" > </br>
+    </td>
+   <td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"]@\"></td></tr>
+     
+	 """
+      }
+	if (item.equals("Clone Firewall Policy")) {
+        html_to_be_rendered = """
+        ${html_to_be_rendered}
+
+       <tr>
+   <td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"ins_fir_pattern_filter=[\"></td>
+    <td><label title=\"service1-label\" class=\" \">Enter region of the Firewall Policy to be cloned <b> (Mandatory) </b>: </label></td>
+	<td><input type=\"text\" class=\" \" name=\"value\" > </br></td>      
+   <td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"]@\"></td></tr>
+    <tr>
+	<td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"ins_fir_pattern_filter=[\"></td>
+    <td><label title=\"service1-label\" class=\" \">Enter name of the Compartment (as it appears in OCI) of the Firewall Policy to be cloned <b> (Mandatory) </b> : </label></td>
+	<td><input type=\"text\" class=\" \" name=\"value\" > </br></td>    
+   <td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"]@\"></td></tr>
+    <tr>
+	<td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"ins_fir_pattern_filter=[\"></td>
+    <td><label title=\"service1-label\" class=\" \">Enter names of the Firewalls (comma separated) for which you need to clone their latest attached policy <b> (Mandatory) </b> : </label></td>
+	<td><input type=\"text\" class=\" \" name=\"value\" > </br></td>    
+   <td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"]@\"></td></tr>
+   <tr>
+	<td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"ins_fir_pattern_filter=[\"></td>
+    <td><label title=\"service1-label\" class=\" \">Enter names for the cloned policies (comma separated), in the same order as that of firewalls in the above text box : </label></td>
+	<td><input type=\"text\" class=\" \" name=\"value\" > </br></td>
+    <td>(Leave empty if you need tool to generate the policy names) </td>    
+   <td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"]@\"></td></tr>
+     
+	 """
+      }
+	if (item.equals("Delete Firewall Policy")) {
+        html_to_be_rendered = """
+        ${html_to_be_rendered}
+
+       <tr>
+   <td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"ins_fir_pattern_filter=[\"></td>
+    <td><label title=\"service1-label\" class=\" \">Enter region of the Firewall Policy to be deleted <b> (Mandatory) </b>: </label></td>
+	<td><input type=\"text\" class=\" \" name=\"value\" > </br></td>      
+   <td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"]@\"></td></tr>
+    <tr>
+	<td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"ins_fir_pattern_filter=[\"></td>
+    <td><label title=\"service1-label\" class=\" \">Enter name of the Compartment (as it appears in OCI) of the Firewall Policy to be deleted <b> (Mandatory) </b> : </label></td>
+	<td><input type=\"text\" class=\" \" name=\"value\" > </br></td>    
+   <td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"]@\"></td></tr>
+    <tr>
+	<td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"ins_fir_pattern_filter=[\"></td>
+    <td><label title=\"service1-label\" class=\" \">Enter names of the Firewall Policies(comma separated) that need to be deleted <b> (Mandatory) </b> : </label></td>
+	<td><input type=\"text\" class=\" \" name=\"value\" > </br></td>    
+   <td><input type=\"hidden\" id=\"sep1\" name=\"value\" value=\"]@\"></td></tr>
+
+     
+	 """
+      }
    if (item.equals("Export Block Volumes/Block Backup Policy")) {
       html_to_be_rendered = """
       ${html_to_be_rendered}
--- jenkins_install/scriptler/scripts/SubChildOptions.groovy
+++ jenkins_install/scriptler/scripts/SubChildOptions.groovy
@@ -1,5 +1,6 @@
 List sec_rules = ["<b>SECURITY RULES</b>:disabled","Export Security Rules (From OCI into SecRulesinOCI sheet)", "Add/Modify/Delete Security Rules (Reads SecRulesinOCI sheet)"]
 List route_rules = ["<b>ROUTE RULES</b>:disabled","Export Route Rules (From OCI into RouteRulesinOCI sheet)", "Add/Modify/Delete Route Rules (Reads RouteRulesinOCI sheet)"]
+List firewall_policy = ["<b>FIREWALL POLICY</b>:disabled","Add/Modify Policy", "Add/Modify Service","Add/Modify Service-list","Add/Modify Application","Add/Modify Application-list","Add/Modify Address-list","Add/Modify Url-list","Add/Modify Security rules","Add/Modify Mapped Secrets","Add/Modify Decryption Rules","Add/Modify Decryption Profile"]
 List drg_route_rules = ["<b>DRG ROUTE RULES</b>:disabled","Export DRG Route Rules (From OCI into DRGRouteRulesinOCI sheet)", "Add/Modify/Delete DRG Route Rules (Reads DRGRouteRulesinOCI sheet)"]
 List nsg = ["<b>NSGs</b>:disabled","Export NSGs (From OCI into NSGs sheet)", "Add/Modify/Delete NSGs (Reads NSGs sheet)"]
 List cis =  ["<b>CIS</b>:disabled","CD3 Image already contains the latest CIS compliance checking script available at the time of cd3 image release. Download latest only if new version of the script is available", "Execute compliance checking script"]
@@ -20,6 +21,9 @@ for (item in SubOptions.split(",")) {
     }
     if (item.equals("CIS Compliance Checking Script")){
         final_list += cis
+    }
+      if (item.equals("Add/Modify Firewall Policy")){
+        final_list += firewall_policy
     }
 }
 return final_list
