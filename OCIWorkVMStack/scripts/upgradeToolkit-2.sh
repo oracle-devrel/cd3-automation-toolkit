d46548c3b3af83315e5e12538908000f4f3498ad
bhlohumi <bhanu.prakash.lohumi@oracle.com>
2025-06-26 22:26:47 +0530

    upgrate toolkit update

--- OCIWorkVMStack/scripts/upgradeToolkit-2.sh
+++ OCIWorkVMStack/scripts/upgradeToolkit-2.sh
@@ -7,9 +7,10 @@ logfile="/tmp/upgradeToolkit-2.log_"$NOW
 
 # Redirect all output to log file
 export LOG_FILE=$logfile
-exec > $LOG_FILE
-exec 2>&1
-echo "redirecting all logs to $LOG_FILE"
+exec 3>&1
+exec > "$logfile" 2>&1
+# print this to terminal
+echo "redirecting all logs to $LOG_FILE" >&3
 
 stop_exec () {
 if [[ $? -ne 0 ]] ; then
@@ -51,6 +52,7 @@ function copy_data_from_variables_file() {
 
 sudo git clone https://github.com/oracle-devrel/cd3-automation-toolkit.git -b testUpgrade-container /tmp/temp
 #Get version from latest tag
+git config --global color.ui false
 git config --global --add safe.directory /tmp/temp
 cd /tmp/temp
 version=$(git describe --tags)
@@ -63,9 +65,9 @@ name="cd3_toolkit_"$version
 
 #################### Run createTenancyConfig.py for new version and create Prefixes ################
 #sudo read -p "Enter Current Version to be upgraded: " current_version > /dev/tty
-echo -n "Enter Current Version to be upgraded: " > /dev/tty
+echo -n "Enter Current Version to be upgraded: " >&3
+# read input from terminal
 read current_version < /dev/tty
-echo $current_version
 current_name="cd3_toolkit_"$current_version
 
 if [ ! -d "/$userdir/$current_version" ] && sudo podman container exists "$current_name"; then
@@ -75,10 +77,9 @@ else
   exit 1
 fi
 
-#sudo read -p "Enter comma separated prefix names to be ungraded: " current_prefixes > /dev/tty
-echo -n "Enter comma separated prefix names to be ungraded: " > /dev/tty
+#sudo read -p "Enter comma separated prefix names to be upgraded: " current_prefixes > /dev/tty
+echo -n "Enter comma separated prefix names to be upgraded: " >&3
 read current_prefixes < /dev/tty
-echo $current_prefixes
 
 # Setting IFS (input field separator) value as ","
 IFS=','
@@ -101,17 +102,17 @@ do
   sudo chown -R $username:$username /$username/$version
   sudo chown -R $username:$username /$username/$current_version
   echo "--------------------------------------------------"
-  echo "Running createTenancyConfig.py for prefix: $prefix"
+  echo "Running createTenancyConfig.py for prefix: $prefix" >&3
   sudo podman exec -it $name bash -c "cd /${username}/oci_tools/cd3_automation_toolkit/user-scripts; python createTenancyConfig.py /${username}/tenancies/${prefix}/.config_files/${prefix}_tenancyconfig.properties --upgradeToolkit True" 2>&1 > /dev/tty
   echo "createTenancyConfig.py ended for prefix: $prefix"
   echo "--------------------------------------------------"
 
   echo -e "Checking GIT repo configuration for prefix $prefix  and version $current_version"
-  current_git_status=$(sudo podman exec -it $current_name bash -c "cd /${username}/tenancies/${prefix}/terraform_files/; git status")
+  current_git_status=$(sudo podman exec -it $current_name bash -c "cd /${username}/tenancies/${prefix}/terraform_files/; git config --global color.ui false; git status")
   echo $current_git_status
 
   echo -e "Checking GIT repo configuration for prefix $prefix  and version $version"
-  git_status=$(sudo podman exec -it $name bash -c "cd /${username}/tenancies/${prefix}/terraform_files/; git status")
+  git_status=$(sudo podman exec -it $name bash -c "cd /${username}/tenancies/${prefix}/terraform_files/; git config --global color.ui false; git status")
   echo $git_status
 
   text="fatal: not a git repository"
@@ -126,7 +127,7 @@ do
     echo "\nGIT status not same for both versions for this prefix $prefix. Exiting!!!\n"
   fi
 
-  echo "Copying tfvars for prefix: $prefix from $current_version to $version and running terraform plan"
+  echo "Copying tfvars for prefix: $prefix from $current_version to $version and running terraform plan" >&3
 
   dest_dir=/${username}/${version}/${prefix}/terraform_files
   cd /${username}/${current_version}/${prefix}/terraform_files
@@ -140,9 +141,11 @@ do
   # copy all the tfvars to destination path and store directories to unique_dirs file
   sudo find . -type f -name "*.auto.tfvars" -exec bash -c '
   dest="$1/$2"
+
   dest_dir_on_vm=$(dirname "$dest")
   if [ -d "$dest_dir_on_vm" ]; then
     sudo cp "$2" "$dest"
+
     echo $dest_dir_on_vm >> /tmp/unique_dirs
   fi
   ' -- "$dest_dir" {} \;
@@ -153,7 +156,7 @@ do
   # process each line from unique_dirs file
   if [ "$git" == 1 ]; then
     echo -e "\ncheckout develop back in source dir"
-    sudo podman exec -it $current_name bash -c "cd /${username}/tenancies/${prefix}/terraform_files/; git checkout develop"
+    sudo podman exec -it $current_name bash -c "cd /${username}/tenancies/${prefix}/terraform_files/; git checkout develop;git config --global color.ui true"
   fi
 
   # Read file into an array
@@ -167,10 +170,11 @@ do
 
     source_dir=$(echo "$dir" | sed "s/$version/tenancies/g")
     source_dir_on_vm=$(echo "$dir" | sed "s/${version}/${current_version}/g")
-
+    echo $source_dir >&3
     # Run Terraform in source container
     sudo podman exec -i "$current_name" bash -c "
       cd \"$source_dir\" || exit 1
+      export TF_CLI_ARGS=-no-color
       terraform init
       terraform state pull > source_tfstate
       terraform state list > resources_list
@@ -182,13 +186,14 @@ do
     echo "Dest dir: $dest_dir"
 
     # Copy state files from source VM directory
-    sudo cp "$source_dir_on_vm/source_tfstate" "$dir/" || { echo "Failed to copy source_tfstate"; continue; }
-    sudo cp "$source_dir_on_vm/resources_list" "$dir/" || { echo "Failed to copy resources_list"; continue; }
+    sudo mv "$source_dir_on_vm/source_tfstate" "$dir/" || { echo "Failed to copy source_tfstate"; continue; }
+    sudo mv "$source_dir_on_vm/resources_list" "$dir/" || { echo "Failed to copy resources_list"; continue; }
     sudo chown -R "$username:$username" "/$username/$version"
 
     # Run Terraform in destination container
     sudo podman exec -i "$name" bash -c "
       cd \"$dest_dir\" || exit 1
+      export TF_CLI_ARGS=-no-color
       terraform init
       terraform state pull > dest_tfstate
       while IFS= read -r r_import || [[ -n \"\$r_import\" ]]; do
@@ -232,11 +237,11 @@ do
 
     START_MARKER="#START_oke_source_ocids#"
     END_MARKER="#oke_source_ocids_END#"
-    copy_data_from_variables_file $START_MARKER $END_MARKER $SRC_FILE $DEST_FILE
+    copy_data_from_variables_file
 
     START_MARKER="#START_oke_ssh_keys#"
     END_MARKER="#oke_ssh_keys_END#"
-    copy_data_from_variables_file $START_MARKER $END_MARKER $SRC_FILE $DEST_FILE
+    copy_data_from_variables_file
 
     START_MARKER="#START_sddc_ssh_keys#"
     END_MARKER="#sddc_ssh_keys_END#"
@@ -258,8 +263,9 @@ do
   echo "Done Configuring prefix $prefix"
   if [ "$git" == 1 ]; then
     echo -e "\nPushing local files to GIT Repo"
-    sudo podman exec -it $name bash -c "cd /${username}/tenancies/${prefix}/terraform_files/; git add .; git commit -m 'Push from upgrade script to new version GIT Repo'; git push --set-upstream origin develop; git checkout main; git pull origin main; git merge develop; git push origin main; git checkout develop"
+    sudo podman exec -it $name bash -c "cd /${username}/tenancies/${prefix}/terraform_files/; git config --global color.ui false;git add .; git commit -m 'Push from upgrade script to new version GIT Repo'; git push --set-upstream origin develop; git checkout main; git pull origin main; git merge develop; git push origin main; git checkout develop;git config --global color.ui true"
   fi
   echo "--------------------------------------------------"
 
 done
+sed -i 's/\r//g' $LOG_FILE
