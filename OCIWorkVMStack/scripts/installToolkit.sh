ec91ff46c2f720b348489dd5b63f8342d7791455
Suruchi <suruchi.singla@oracle.com>
2026-03-26 23:51:02 +0530

    Update installToolkit.sh

--- OCIWorkVMStack/scripts/installToolkit.sh
+++ OCIWorkVMStack/scripts/installToolkit.sh
@@ -88,7 +88,7 @@ runcmd:
     sudo podman --version >> $logfile 2>&1 || true
 
     echo "***Download Toolkit***" >> $logfile 2>&1
-    sudo git clone https://github.com/oracle-devrel/cd3-automation-toolkit.git -b develop $toolkit_dir >> $logfile 2>&1
+    sudo git clone https://github.com/oracle-devrel/cd3-automation-toolkit.git $toolkit_dir >> $logfile 2>&1
     cp -r $toolkit_dir/cd3_automation_toolkit /$mount_dir/oci_tools/
     cp -r $toolkit_dir/othertools /$mount_dir/oci_tools/
     sudo chown -R $username:$username /$mount_dir/oci_tools/
